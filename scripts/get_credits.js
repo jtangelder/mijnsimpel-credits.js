bc585fcd4156281f131557a9ab073c726916198c
Jorik Tangelder <j.tangelder@gmail.com>
2013-03-10 17:32:16 +0100

    things

--- scripts/get_credits.js
+++ scripts/get_credits.js
@@ -3,9 +3,12 @@ var casper = require('casper').create(),
 
 var source_url = 'https://www.mijnsimpel.nl/login.aspx?ReturnUrl=Credit.aspx',
     credits = [];
+    // get the sign in credentials
     credentials = JSON.parse(fs.read('scripts/credentials.json'));
 
+
 casper.start(source_url, function() {
+    // sign in
 	this.fill('form', {
 		'ctl00$content$txtLoginName': credentials.username,
 		'ctl00$content$txtPassword': credentials.password
@@ -14,8 +17,19 @@ casper.start(source_url, function() {
 	this.click('#ctl00_content_lbSubmit');
 });
 
+
 casper.then(function() {
     credits = this.evaluate(function() {
+        // simple formatting
+        function formatLabel(label) {
+            return label.replace("nog te gebruiken", "Resterende");
+        }
+
+        function formatValue(value) {
+            return value.replace(' (uren/min/sec)', '');
+        }
+
+        // find the credits table and gather the data
         var data = [],
             table = document.querySelector("#content table"),
             row, r;
@@ -23,15 +37,18 @@ casper.then(function() {
         for(r=0; r<table.rows.length; r++) {
             row = table.rows[r];
             data.push({
-                label: row.cells[0].innerText,
-                value: row.cells[1].innerText
+                label: formatLabel(row.cells[0].innerText),
+                value: formatValue(row.cells[1].innerText)
             });
         }
+
 		return data;
 	});
 });
 
+
 casper.run(function() {
+    // print the credits
 	this.echo(JSON.stringify(credits));
 	this.exit();
 });
